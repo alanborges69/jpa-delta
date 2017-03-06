9c33586d24fd8157b9709f82e3979a879791a6c5
atilla8huno <atilla8huno@gmail.com>
2017-03-05 21:04:37 -0300

    Refatorando teste unitário da classe JPAUtil;

--- src/test/java/br/edu/faculdadedelta/util/test/JPAUtilTest.java
+++ src/test/java/br/edu/faculdadedelta/util/test/JPAUtilTest.java
@@ -5,7 +5,9 @@ import javax.persistence.EntityManager;
 import static org.junit.Assert.*;
 
 import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.edu.faculdadedelta.util.JPAUtil;
@@ -14,26 +16,39 @@ public class JPAUtilTest {
 
 	private static final Logger LOGGER = Logger.getLogger(JPAUtilTest.class);
 	
+	private EntityManager em;
+	
+	@Before
+	public void instanciarEntityManager() {
+		em = JPAUtil.INSTANCE.getEntityManager();
+	}
+	
+	@After
+	public void fecharEntityManager() {
+		if (em.isOpen()) {
+			em.close();
+		}
+	}
+	
 	@Test
-	public void deveInstanciarEntityManager() {
-		EntityManager em = JPAUtil.INSTANCE.getEntityManager();
-		
+	public void deveTerInstanciaDoEntityManagerDefinida() {
 		assertNotNull("instância do EntityManager não deve estar nula", em);
-		
+	}
+	
+	@Test
+	public void deveFecharEntityManager() {
 		em.close();
+		
+		assertFalse("instância do EntityManager não deve estar nula", em.isOpen());
 	}
 	
 	@Test
 	public void deveAbrirUmaTransacao() {
-		EntityManager em = JPAUtil.INSTANCE.getEntityManager();
-		
 		assertFalse("transação deve estar fechada", em.getTransaction().isActive());
 		
 		em.getTransaction().begin();
 		
 		assertTrue("transação deve estar aberta", em.getTransaction().isActive());
-		
-		em.close();
 	}
 	
 	@AfterClass
